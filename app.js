76405e066578cf66d0dfdd625f5454ad59c63a63
timmah12341 <timfinke01@gmail.com>
2026-02-14 11:00:08 +0100

    Adopt legacy upgrades array format with ids, costs, adds, and icons

--- app.js
+++ app.js
@@ -32,12 +32,59 @@
   }
 
   // ---------- Data ----------
-  var UPGRADES = {
-    cursor: { name: 'Cursor', base: 10, cps: 1 },
-    factory: { name: 'Factory', base: 120, cps: 7 },
-    lab: { name: 'Lab', base: 1100, cps: 55 },
-    portal: { name: 'Portal', base: 9000, cps: 430 }
-  };
+  var upgrades = [
+    { id: 'u0.5', name: 'bacteriophage', baseCost: 1, add: 0.1, icon: 'upgrade0.1.png' },
+    { id: 'u1', name: 'Tim-ema', baseCost: 100, add: 2, icon: 'upgrade1.png' },
+    { id: 'u1.5', name: 'chezburger', baseCost: 250, add: 4, icon: 'upgrade1.5.png' },
+    { id: 'u2', name: 'Floatie', baseCost: 500, add: 8, icon: 'upgrade2.png' },
+    { id: 'u2.5', name: 'mier', baseCost: 1000, add: 15, icon: 'upgrade2.5.png' },
+    { id: 'u3', name: ':3', baseCost: 2000, add: 25, icon: 'upgrade3.png' },
+    { id: 'u3.5', name: 'Shoarma Broodje', baseCost: 4000, add: 40, icon: 'upgrade3.5.png' },
+    { id: 'u4', name: 'Tim', baseCost: 6000, add: 70, icon: 'upgrade4.png' },
+    { id: 'u4.5', name: 'Teh Epic Tim is comming!!!', baseCost: 10000, add: 120, icon: 'upgrade4.5.png' },
+    { id: 'u5', name: 'Depression Upgrade', baseCost: 15000, add: 180, icon: 'upgrade5.png' },
+    { id: 'u5.5', name: 'Une petite Biertje', baseCost: 25000, add: 350, icon: 'upgrade5.5.png' },
+    { id: 'u6', name: 'Ball Guy Tim', baseCost: 40000, add: 450, icon: 'upgrade6.png' },
+    { id: 'u6.5', name: 'Philips stofzuiger D380', baseCost: 70000, add: 700, icon: 'upgrade6.5.png' },
+    { id: 'u7', name: 'Johan', baseCost: 120000, add: 1200, icon: 'upgrade7.png' },
+    { id: 'u8', name: 'Tequilla', baseCost: 300000, add: 2500, icon: 'upgrade8.png' },
+    { id: 'u9', name: 'Golden Tim', baseCost: 1000000, add: 50000, icon: 'upgrade9.png' },
+    { id: 'u10', name: 'Minecraft Steve', baseCost: 5000000, add: 100000, icon: 'upgrade10.png' },
+    { id: 'u11', name: 'ADMIN ABUSE TIM', baseCost: 10000000, add: 500000, icon: 'upgrade11.png' },
+    { id: 'u12', name: 'Nuclear Power Plant', baseCost: 50000000, add: 1000000, icon: 'upgrade12.png' },
+    { id: 'u13', name: 'Tim Clicker™: Ultimate Clicker V.I.P. Edition Deluxe 4.2 free', baseCost: 100000000, add: 5000000, icon: 'upgrade13.png' },
+    { id: 'u14', name: 'Engineer monke', baseCost: 500000000, add: 10000000, icon: 'upgrade14.png' },
+    { id: 'u15', name: 'Hamster Vuurpijl', baseCost: 1000000000, add: 50000000, icon: 'upgrade15.png' },
+    { id: 'u16', name: 'Hatsune Timu', baseCost: 5000000000, add: 100000000, icon: 'upgrade16.png' },
+    { id: 'u17', name: 'TimTimmer', baseCost: 10000000000, add: 500000000, icon: 'upgrade17.png' },
+    { id: 'u18', name: 'Blackhole', baseCost: 50000000000, add: 1000000000, icon: 'upgrade18.png' },
+    { id: 'u19', name: ':wave1: :wave2: :wave3: :wave4:', baseCost: 100000000000, add: 5000000000, icon: 'upgrade19.png' },
+    { id: 'u20', name: 'Tim co.™', baseCost: 500000000000, add: 10000000000, icon: 'upgrade20.png' },
+    { id: 'u21', name: 'White Hole', baseCost: 1000000000000, add: 50000000000, icon: 'upgrade21.png' },
+    { id: 'u22', name: 'DEATH STARE TIM', baseCost: 5000000000000, add: 100000000000, icon: 'upgrade22.png' },
+    { id: 'u23', name: 'Blooket Tim', baseCost: 10000000000000, add: 500000000000, icon: 'upgrade23.png' },
+    { id: 'u24', name: 'Bordspel Geuzen', baseCost: 50000000000000, add: 1000000000000, icon: 'upgrade24.png' },
+    { id: 'u25', name: 'Magnus Carlsen', baseCost: 100000000000000, add: 500000000000, icon: 'upgrade25.png' },
+    { id: 'u26', name: 'Neutronenster', baseCost: 500000000000000, add: 1000000000000, icon: 'upgrade26.png' },
+    { id: 'u27', name: 'Supernova', baseCost: 1000000000000000, add: 5000000000000, icon: 'upgrade27.png' },
+    { id: 'u28', name: 'Polonium-210', baseCost: 5000000000000000, add: 10000000000000, icon: 'upgrade28.png' },
+    { id: 'u29', name: 'Dyson Shell', baseCost: 10000000000000000, add: 50000000000000, icon: 'upgrade29.png' },
+    { id: 'u30', name: 'Particle Accelerator', baseCost: 50000000000000000, add: 100000000000000, icon: 'upgrade30.png' },
+    { id: 'u31', name: 'The heat death of the timiverse', baseCost: 100000000000000000, add: 500000000000000, icon: 'upgrade31.png' },
+    { id: 'u32', name: 'Strange Quark', baseCost: 500000000000000000, add: 1000000000000000, icon: 'upgrade32.png' },
+    { id: 'u33', name: 'The Great Attractor', baseCost: 1000000000000000000, add: 5000000000000000, icon: 'upgrade33.png' },
+    { id: 'u34', name: 'The universe is a doughnut theory', baseCost: 5000000000000000000, add: 10000000000000000, icon: 'upgrade34.png' },
+    { id: 'u35', name: 'Lobotomy', baseCost: 50000000000000000000, add: 100000000000000000, icon: 'upgrade37.png' },
+    { id: 'u36', name: 'Dafthusky', baseCost: 100000000000000000000, add: 500000000000000000, icon: 'upgrade39.png' },
+    { id: 'u37', name: 'Quasar', baseCost: 500000000000000000000, add: 1000000000000000000, icon: 'upgrade35.png' },
+    { id: 'u38', name: 'Who is the inventor of cheese???', baseCost: 1000000000000000000000, add: 5000000000000000000, icon: 'upgrad.png' },
+    { id: 'u39', name: 'The fabric of space and Tim', baseCost: 5000000000000000000000, add: 10000000000000000000, icon: 'upgrade36.png' },
+    { id: 'u40', name: 'Deep Brain stimulation', baseCost: 10000000000000000000000, add: 50000000000000000000, icon: 'upgrade38.png' },
+    { id: 'u41', name: '6 laws of quantum physics', baseCost: 50000000000000000000000, add: 100000000000000000000, icon: 'upgrade40.png' },
+    { id: 'u42', name: 'Weapon of Mosquito Destruction', baseCost: 100000000000000000000000, add: 500000000000000000000, icon: 'upgrade41.png' }
+  ];
+  var UPGRADES = upgrades;
+
 
   var SKINS = [];
   // Includes all current skin art files (assets/skins + root legacy skins), duplicated on purpose.
@@ -153,8 +200,9 @@
 
   function cps() {
     var total = 0;
-    for (var id in state.upgrades) {
-      if (UPGRADES[id]) total += UPGRADES[id].cps * state.upgrades[id];
+    for (var i = 0; i < UPGRADES.length; i++) {
+      var up = UPGRADES[i];
+      total += (state.upgrades[up.id] || 0) * up.add;
     }
     total *= currentSkin().mult;
     total *= (1 + state.rebirths * 0.25);
@@ -242,21 +290,21 @@
   function renderUpgrades() {
     var box = el('upgradeShop');
     box.innerHTML = '';
-    for (var id in UPGRADES) {
-      (function (upgradeId) {
-        var owned = state.upgrades[upgradeId] || 0;
-        var price = upgradeCost(UPGRADES[upgradeId].base, owned);
+    for (var i = 0; i < UPGRADES.length; i++) {
+      (function (up) {
+        var owned = state.upgrades[up.id] || 0;
+        var price = upgradeCost(up.baseCost, owned);
         var btn = document.createElement('button');
-        btn.textContent = UPGRADES[upgradeId].name + ' (' + owned + ') - ' + price;
+        btn.textContent = up.name + ' (' + owned + ') - ' + price + ' [' + up.icon + ']';
         btn.onclick = function () {
           if (state.tims < price) return;
           state.tims -= price;
-          state.upgrades[upgradeId] = owned + 1;
+          state.upgrades[up.id] = owned + 1;
           saveNow();
           renderAll();
         };
         box.appendChild(btn);
-      })(id);
+      })(UPGRADES[i]);
     }
   }
 
