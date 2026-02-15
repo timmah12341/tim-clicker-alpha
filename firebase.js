8857793b50c7f14ce8f403dfec00a26144496eed
timmah12341 <timfinke01@gmail.com>
2026-02-15 11:58:14 +0100

    Refactor firebase configuration and import statements

--- firebase.js
+++ firebase.js
@@ -1,17 +1,38 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, get, set, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { 
+  getDatabase, ref, get, set, update 
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import {
+  getAuth,
+  signInAnonymously,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-const firebaseConfig = {};
-firebaseConfig.apiKey = "AIzaSyDleRW-O4yP9FJhuqQtMTVT0c_Dd1PPA98";
-firebaseConfig.authDomain = "tim-clicker-alpha.firebaseapp.com";
-firebaseConfig.databaseURL = "https://tim-clicker-alpha-default-rtdb.europe-west1.firebasedatabase.app";
-firebaseConfig.projectId = "tim-clicker-alpha";
-firebaseConfig.storageBucket = "tim-clicker-alpha.firebasestorage.app";
-firebaseConfig.messagingSenderId = "40617780569";
-firebaseConfig.appId = "1:40617780569:web:1a82146a3554ab1e365848";
+const firebaseConfig = {
+  apiKey: "AIzaSyDleRW-O4yP9FJhuqQtMTVT0c_Dd1PPA98",
+  authDomain: "tim-clicker-alpha.firebaseapp.com",
+  databaseURL: "https://tim-clicker-alpha-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tim-clicker-alpha",
+  storageBucket: "tim-clicker-alpha.firebasestorage.app",
+  messagingSenderId: "40617780569",
+  appId: "1:40617780569:web:1a82146a3554ab1e365848"
+};
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getDatabase(app);
+const auth = getAuth(app);
+
+export let currentUID = null;
+
+signInAnonymously(auth);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUID = user.uid;
+    console.log("Signed in:", currentUID);
+  }
+});
 
 export async function loadPlayer(uid) {
   const snap = await get(ref(db, "players/" + uid));
